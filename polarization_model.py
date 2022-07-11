ca3b1ca7bce5a96706496170326833f505f9f802
Bastian.hundt <bastian@hundt.me>
2022-07-11 21:24:31 +0200

    working version

--- polarization_model.py
+++ polarization_model.py
@@ -8,8 +8,8 @@ class Person(ap.Agent):
 
     def setup(self):
         self.polarization = 0.0
-        self.initial_polarization = self.polarization
-        self.group = 0
+        #self.initial_polarization = self.polarization
+        #self.group = 0
         pass
 
     def _move_to(self, partner_pol):
@@ -30,20 +30,20 @@ class Person(ap.Agent):
 
 
     def update_polarization(self):
-        # we model self interest as the tendency to move to ones initial starting point
-        if self.model.nprandom.random() <= self.model.p.P:
-            self._move_to(self.initial_polarization)
+        # # we model self interest as the tendency to move to ones initial starting point
+        # if self.model.nprandom.random() <= self.model.p.P:
+        #     self._move_to(self.initial_polarization)
 
         # pick a random agent
         partner = self.model.agents.random().to_list()[0]
         partner_dist = np.abs( self.polarization - partner.polarization)
         
-        # with a certain prob. we will just be repulsed by a member of the opposite group regardless of distance; similiarily we will be attracted by a member of our own group
-        if (self.model.nprandom.random() <= self.model.p.A) and (self.group != 0):
-            if (self.group == partner.group):
-                self._move_to(partner.polarization)
-            if (self.group != partner.group):
-                self._move_away(partner.polarization) 
+        # # with a certain prob. we will just be repulsed by a member of the opposite group regardless of distance; similiarily we will be attracted by a member of our own group
+        # if (self.model.nprandom.random() <= self.model.p.A) and (self.group != 0):
+        #     if (self.group == partner.group):
+        #         self._move_to(partner.polarization)
+        #     if (self.group != partner.group):
+        #         self._move_away(partner.polarization) 
 
         # check whether we can interact, interaction prob is ~ (1/2)^(d/E)
         interaction_prob = math.pow(1/2, partner_dist / self.model.p.E)
@@ -65,8 +65,8 @@ class PolarizationModel(ap.Model):
         # init polarization change
         for agent in self.agents:
             agent.polarization = self.nprandom.normal(loc=0.5, scale= 0.2)
-            agent.initial_polarization = agent.polarization
-            agent.group = self.random.choice([-1, 0, 1])
+            #agent.initial_polarization = agent.polarization
+            #agent.group = self.random.choice([-1, 0, 1])
 
     def step(self):
         self.agents.update_polarization()
--- streamlit_app.py
+++ streamlit_app.py
@@ -7,51 +7,15 @@ import numpy as np
 from polarization_model import PolarizationModel
 
 
-# def main():
-#     params = {
-#         'agents': 100,
-#         'steps': 10000,
-#         #'seed': 42,
-#         'E': 0.1,       # exposure porbability
-#         'T': 0.25,      # tolerance window
-#         'R': 0.25,      # movement coefficient
-#         'P': -0.99,       # self interest factor
-#         'A': -0.99,       # affective polarization
-#     }
-
-#     model = PolarizationModel(params)
-#     result = model.run()
-#     #print(result.variables.PolarizationModel)
-
-#     # plot polarization over time
-#     df = result.variables.PolarizationModel
-#     plt.plot(df.index, df['Var'], '.')
-#     plt.show()
-#     plt.close()
-
-#     # plot polarization histogram
-#     hist_values, bins = df.iloc[-1]['Hist'], df.iloc[-1]['Bins']
-#     width = 0.7 * (bins[1] - bins[0])
-#     center = (bins[:-1] + bins[1:]) / 2
-
-#     plt.bar(center, hist_values, align='center', width=width)
-#     plt.show()
-#     plt.close()
-
-# if __name__ == "__main__":
-#     main()
-
 # streamlit application
 def run_simulation(agents: int, steps: int, exposure_prob: float, tolerance_window: float, movement_coefficient: float):
     params = {
         'agents': agents,
         'steps': steps,
-        #'seed': 42,
+        'seed': 42,
         'E': exposure_prob,       # exposure prob
         'T': tolerance_window,      # tolerance window
         'R': movement_coefficient,      # movement coefficient
-        'P': -0.99,       # self interest factor
-        'A': -0.99,       # affective polarization
     }
 
     model = PolarizationModel(params)
@@ -62,8 +26,8 @@ def run_simulation(agents: int, steps: int, exposure_prob: float, tolerance_wind
 st.title('Societal Polarization Simulation')
 
 st.subheader('Simulation Settings')
-agents = st.slider('Number of Simulated Agents', 1, 200, 100)
-steps = st.select_slider('Number of Simulation Steps', range(0, 20500, 500), 2000)
+agents = st.slider('Number of Simulated Agents', 1, 200, 50)
+steps = st.select_slider('Number of Simulation Steps', range(0, 20500, 500), 5000)
 
 exp = st.slider('Exposure Probability', 0.0, 1.0, 0.1)
 tol = st.slider('Tolerance Window', 0.0, 1.0, 0.25)
