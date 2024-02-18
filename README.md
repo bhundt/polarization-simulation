3a201a9507398de803aea1a5501afcfdf6156adf
Bastian.hundt <bastian.hundt@gmx.de>
2024-02-18 18:57:28 +0100

    requirements update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Political Polarization Simulation using Streamlit
-In the paper ["Preventing extreme polarization of political attitudes"](https://www.pnas.org/content/118/50/e2102139118) the authors present a model of political polarization in which individuals are influenced by the opinions of others.
+In the paper ["Preventing extreme polarization of political attitudes"](https://www.pnas.org/content/118/50/e2102139118) by Axelrod et. al. the authors present a model of political polarization in which individuals are influenced by the opinions of others.
 
 This repository contains a Streamlit app that simulates the model presented in the paper. The app allows you to change the parameters of the model and see how the political polarization evolves over time. 
 
@@ -7,5 +7,17 @@ This repository contains a Streamlit app that simulates the model presented in t
 
 See this blogpost for more details: [Political Polarization Simulation using Streamlit](). 
 
-## Local Setup
-Start simulation by executing ```streamlit run streamlit_app.py```
+## Local setup & run
+The code was tested with Python 3.9.5 and 3.11.7. 
+
+Create a virtual environment and install the required packages:
+```bash
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+Start simulation app with Streamlit:
+```bash
+streamlit run streamlit_app.py
+```
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,25 @@
 import streamlit as st
-import agentpy as ap
-import multiprocessing as mp
-import matplotlib.pyplot as plt
-import numpy as np
 
 from polarization_model import PolarizationModel
 
 
-# streamlit application
 def run_simulation(
     agents: int, steps: int, exposure_prob: float, tolerance_window: float, movement_coefficient: float
-):
+) -> PolarizationModel:
+    """
+    Run a simulation of the polarization model.
+
+    Parameters:
+    agents (int): The number of agents in the simulation.
+    steps (int): The number of steps to run the simulation for, can be interpreted as time.
+    exposure_prob (float): The probability of agents being exposed to opposing opinions.
+    tolerance_window (float): The tolerance window within which agents accept opposing opinions.
+    movement_coefficient (float): The movement coefficient that determines the extent of agent movement.
+
+    Returns:
+    result.variables.PolarizationModel: The result of the simulation, including the polarization model variables.
+    """
+
     params = {
         "agents": agents,
         "steps": steps,
@@ -32,18 +41,18 @@ st.markdown(
 )
 
 st.subheader("Simulation Settings")
-agents = st.slider("Number of Simulated Agents", 1, 200, 50)
+agents = st.slider("Number of Simulated Agents", 1, 100, 30)
 steps = st.select_slider("Number of Simulation Steps", range(0, 20500, 500), 5000)
 
-exp = st.slider("Exposure Probability", 0.0, 1.0, 0.1)
-tol = st.slider("Tolerance Window", 0.0, 1.0, 0.25)
-react = st.slider("Reaction Coefficient", 0.0, 1.0, 0.25)
+exp = st.slider("Exposure Probability (how likely is an encounter with a differnt opinion)", 0.0, 1.0, 0.1)
+tol = st.slider("Tolerance Window (tolerance to other opinios)", 0.0, 1.0, 0.25)
+react = st.slider("Reaction Coefficient (strength of reaction to diverging opinions)", 0.0, 1.0, 0.25)
 
 with st.spinner("Simulation running..."):
     df = run_simulation(agents, steps, exp, tol, react)
 st.success("Simulation done!")
 
-st.subheader("Polarization over Time")
+st.subheader("Polarization vs. Time")
 st.line_chart(df.rename(columns={"Var": "Total Polarization"})["Total Polarization"])
 
 st.subheader("Polarization Distribution")
