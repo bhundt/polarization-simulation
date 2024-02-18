939eb968021d09337c17efe23e2be1f3fe664728
Bastian.hundt <bastian.hundt@gmx.de>
2024-02-18 13:43:45 +0100

    readme

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Political Polarization Simulation using Streamlit
 In the paper ["Preventing extreme polarization of political attitudes"](https://www.pnas.org/content/118/50/e2102139118) the authors present a model of political polarization in which individuals are influenced by the opinions of others.
 
-See blogpost for more details: [Political Polarization Simulation using Streamlit](). 
+This repository contains a Streamlit app that simulates the model presented in the paper. The app allows you to change the parameters of the model and see how the political polarization evolves over time. 
 
-## Usage
+[Try the app on Streamlit](https://bhundt-polarization-simulation.streamlit.app).
+
+See this blogpost for more details: [Political Polarization Simulation using Streamlit](). 
+
+## Local Setup
 Start simulation by executing ```streamlit run streamlit_app.py```
--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,16 @@ from polarization_model import PolarizationModel
 
 
 # streamlit application
-def run_simulation(agents: int, steps: int, exposure_prob: float, tolerance_window: float, movement_coefficient: float):
+def run_simulation(
+    agents: int, steps: int, exposure_prob: float, tolerance_window: float, movement_coefficient: float
+):
     params = {
-        'agents': agents,
-        'steps': steps,
-        'seed': 42,
-        'E': exposure_prob,       # exposure prob
-        'T': tolerance_window,      # tolerance window
-        'R': movement_coefficient,      # movement coefficient
+        "agents": agents,
+        "steps": steps,
+        "seed": 42,
+        "E": exposure_prob,  # exposure prob
+        "T": tolerance_window,  # tolerance window
+        "R": movement_coefficient,  # movement coefficient
     }
 
     model = PolarizationModel(params)
@@ -23,22 +25,26 @@ def run_simulation(agents: int, steps: int, exposure_prob: float, tolerance_wind
 
     return result.variables.PolarizationModel
 
-st.title('Societal Polarization Simulation')
 
-st.subheader('Simulation Settings')
-agents = st.slider('Number of Simulated Agents', 1, 200, 50)
-steps = st.select_slider('Number of Simulation Steps', range(0, 20500, 500), 5000)
+st.title("Societal Polarization Simulation")
+st.markdown(
+    'This app simulates the evolution of societal polarization over time as discussed in \nthe paper ["Preventing extreme polarization of political attitudes"](https://www.pnas.org/content/118/50/e2102139118) by Axelrod et. al.'
+)
 
-exp = st.slider('Exposure Probability', 0.0, 1.0, 0.1)
-tol = st.slider('Tolerance Window', 0.0, 1.0, 0.25)
-react = st.slider('Reaction Coefficient', 0.0, 1.0, 0.25)
+st.subheader("Simulation Settings")
+agents = st.slider("Number of Simulated Agents", 1, 200, 50)
+steps = st.select_slider("Number of Simulation Steps", range(0, 20500, 500), 5000)
 
-with st.spinner('Simulation running...'):
+exp = st.slider("Exposure Probability", 0.0, 1.0, 0.1)
+tol = st.slider("Tolerance Window", 0.0, 1.0, 0.25)
+react = st.slider("Reaction Coefficient", 0.0, 1.0, 0.25)
+
+with st.spinner("Simulation running..."):
     df = run_simulation(agents, steps, exp, tol, react)
-st.success('Simulation done!')
+st.success("Simulation done!")
 
-st.subheader('Polarization over Time')
-st.line_chart(df.rename(columns={'Var': 'Total Polarization'})['Total Polarization'])
+st.subheader("Polarization over Time")
+st.line_chart(df.rename(columns={"Var": "Total Polarization"})["Total Polarization"])
 
-st.subheader('Polarization Distribution')
-st.bar_chart(df.iloc[-1]['Hist'])
+st.subheader("Polarization Distribution")
+st.bar_chart(df.iloc[-1]["Hist"])
