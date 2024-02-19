8550f79f5672a8e8d95e8ea49fb9b21c5d6a9553
Bastian.hundt <bastian.hundt@gmx.de>
2024-02-19 19:35:20 +0100

    speling

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,15 +38,17 @@ def run_simulation(
 
 st.title("Societal Polarization Simulation")
 st.markdown(
-    'This app simulates the evolution of societal polarization over time as discussed in \nthe paper ["Preventing extreme polarization of political attitudes"](https://www.pnas.org/content/118/50/e2102139118) by Axelrod et. al. You can find the code on [my github](https://github.com/bhundt/polarization-simulation).'
+    'This app simulates the evolution of societal polarization over time as discussed in \nthe paper ["Preventing extreme polarization of political attitudes"](https://www.pnas.org/content/118/50/e2102139118) by Axelrod et. al. You can find the code on [my github](https://github.com/bhundt/polarization-simulation) and see [my blog]() for more information.'
 )
 
 st.subheader("Simulation Settings")
 agents = st.slider("Number of Agents (number of simulated people)", 1, 100, 30)
-steps = st.select_slider("Number of Simulation Steps (essentially the time the simulation runs)", range(0, 20500, 500), 5000)
+steps = st.select_slider(
+    "Number of Simulation Steps (essentially the time the simulation runs)", range(0, 20500, 500), 5000
+)
 
 exp = st.slider("Exposure Probability (how likely is an encounter with another opinion)", 0.0, 1.0, 0.1)
-tol = st.slider("Tolerance Window (tolerance to other opinios)", 0.0, 1.0, 0.25)
+tol = st.slider("Tolerance Window (tolerance to other opinions)", 0.0, 1.0, 0.25)
 react = st.slider("Reaction Coefficient (strength of reaction to other opinions)", 0.0, 1.0, 0.25)
 
 with st.spinner("Simulation running..."):
@@ -55,7 +57,7 @@ st.success("Simulation done!")
 
 st.subheader("Polarization vs. Time")
 st.markdown(
-    "The plot below shows the total polarization of the society over time. It is calculated using the variance of the polarization of all agents. This means maximum polarization is 0.25."
+    "The plot below shows the total polarization of the society over time. It is calculated using the variance of the polarization of all agents. Maximum polarization is therefore 0.25."
 )
 st.line_chart(df.rename(columns={"Var": "Total Polarization"})["Total Polarization"])
 
